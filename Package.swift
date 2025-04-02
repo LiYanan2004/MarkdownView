adcec29a35160294ae6e18f9fa53188e06c37d53
Yanan Li <1474700628@qq.com>
2025-04-02 13:32:54 +0800

    Update package dependency version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.5.0"),
         .package(url: "https://github.com/raspu/Highlightr.git", from: "2.2.1"),
-        .package(url: "https://github.com/colinc86/LaTeXSwiftUI.git", from: "1.4.0"),
+        .package(url: "https://github.com/colinc86/LaTeXSwiftUI.git", from: "1.4.1"),
     ],
     targets: [
         .target(
