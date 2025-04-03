afa8dacd93f700aae00a8724618ea3e7d7a6a94b
zeroskylian <zeroskylian@gmail.com>
2025-04-03 17:22:42 +0800

    pref: add iOS demo

--- DemoApp/DemoApp/ContentView.swift
+++ DemoApp/DemoApp/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var selection: Tab = .overview
     
     var body: some View {
+#if os(macOS)
         NavigationSplitView {
             List(selection: $selection) {
                 ForEach(TabGroup.allCases) { group in
@@ -28,6 +29,23 @@ struct ContentView: View {
         } detail: {
             selection.destination
         }
+#elseif os(iOS)
+        NavigationStack {
+            List {
+                ForEach(TabGroup.allCases) { group in
+                    Section {
+                        ForEach(group.tabs) { tab in
+                            tab.link
+                        }
+                    } header: {
+                        Text(group.rawValue)
+                    }
+                }
+            }.navigationDestination(for: Tab.self) { item in
+                item.destination
+            }
+        }
+#endif
     }
 }
 
