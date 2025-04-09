df91ebb25cb7905a67cb5c702f6105bf54296678
Yanan Li <1474700628@qq.com>
2025-04-09 15:38:14 +0800

    Remove extra character in math rendering demo

--- DemoApp/DemoApp/Destinations/MathDestination.swift
+++ DemoApp/DemoApp/Destinations/MathDestination.swift
@@ -14,7 +14,7 @@ struct MathDestination: View {
             MarkdownView(#"This sentence uses `$` delimiters to show math inline: $\sqrt{3x-1}+(1+x)^2$"#)
             
             MarkdownView(#"""
-            **The Cauchy-Schwarz Inequality**\
+            **The Cauchy-Schwarz Inequality**
             $$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)$$
             """#)
         }
