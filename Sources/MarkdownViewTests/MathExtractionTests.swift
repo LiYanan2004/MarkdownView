692afd5b4dc88c4ca2636a3a4a3a6e7d33d3af91
Yanan Li <1474700628@qq.com>
2025-04-09 19:42:22 +0800

    Add test cases for math extraction

---
+++ Sources/MarkdownViewTests/MathExtractionTests.swift
@@ -0,0 +1,49 @@
+//
+//  MathExtractionTests.swift
+//  MarkdownView
+//
+//  Created by LiYanan2004 on 2025/4/9.
+//
+
+import Testing
+@testable @_spi(MarkdownViewTesting) import MarkdownView
+
+@MainActor
+struct MathExtractionTests {
+    struct MathExtractionTestConfiguration: Sendable {
+        var plainText: String
+        var extractedMath: [String]
+    }
+    
+    @Test(
+        arguments: [
+            MathExtractionTestConfiguration(
+                plainText: #"delimiters to show math inline: $\sqrt{3x-1}+(1+x)^2$"#,
+                extractedMath: [#"$\sqrt{3x-1}+(1+x)^2$"#]
+            ),
+            MathExtractionTestConfiguration(
+                plainText: #"""
+                **The Cauchy-Schwarz Inequality**
+                $$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)$$
+                """#,
+                extractedMath: [#"$$\left( \sum_{k=1}^n a_k b_k \right)^2 \leq \left( \sum_{k=1}^n a_k^2 \right) \left( \sum_{k=1}^n b_k^2 \right)$$"#]
+            ),
+            MathExtractionTestConfiguration(
+                plainText: #"\( G_{\mu\nu} \): Einstein tensor (spacetime curvature)"#,
+                extractedMath: [#"\( G_{\mu\nu} \)"#]
+            ),
+            MathExtractionTestConfiguration(
+                plainText: #"\[ \hat{H}\psi = E\psi \quad \text{where} \quad \hat{H} = -\frac{\hbar^2}{2m}\nabla^2 + V(\mathbf{r}) \]"#,
+                extractedMath: [#"\[ \hat{H}\psi = E\psi \quad \text{where} \quad \hat{H} = -\frac{\hbar^2}{2m}\nabla^2 + V(\mathbf{r}) \]"#]
+            ),
+        ]
+    )
+    func testMathExtractionCase(
+        _ configuration: MathExtractionTestConfiguration
+    ) async throws {
+        let mathRenderer = MarkdownMathOrTextRenderer(text: configuration.plainText)
+        let extractedMath = mathRenderer.mathRanges
+            .map { String(configuration.plainText[$0]) }
+        #expect(extractedMath == configuration.extractedMath)
+    }
+}
