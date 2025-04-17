f63a64d1841589da90b7fc3a667f7d0e20a13460
Yanan Li <1474700628@qq.com>
2025-04-17 18:31:01 +0800

    Update namings

---
+++ Sources/MarkdownView/Renderers/Math/MathFirstMarkdownViewRenderer.ParsingRangesExtractor.swift
@@ -0,0 +1,65 @@
+//
+//  MathFirstMarkdownViewRenderer.ParsingRangesExtractor.swift
+//  MarkdownView
+//
+//  Created by Yanan Li on 2025/4/17.
+//
+
+import Markdown
+
+extension MathFirstMarkdownViewRenderer {
+    struct ParsingRangesExtractor: MarkupWalker {
+        private var excludedRanges: [Range<SourceLocation>] = []
+        
+        func parsableRanges(in text: String) -> [Range<String.Index>] {
+            var allowedRanges: [Range<String.Index>] = []
+            let excludedRanges = self.excludedRanges.map {
+                ($0.lowerBound.index(in: text)..<$0.upperBound.index(in: text))
+            }
+
+            let fullRange = text.startIndex..<text.endIndex
+            let sortedExcluded = excludedRanges.sorted { $0.lowerBound < $1.lowerBound }
+            var currentStart = fullRange.lowerBound
+            
+            for ex in sortedExcluded {
+                if currentStart < ex.lowerBound {
+                    allowedRanges.append(currentStart..<ex.lowerBound)
+                }
+                currentStart = ex.upperBound
+            }
+            if currentStart < fullRange.upperBound {
+                allowedRanges.append(currentStart..<fullRange.upperBound)
+            }
+            return allowedRanges
+        }
+        
+        mutating func defaultVisit(_ markup: any Markup) {
+            descendInto(markup)
+        }
+        
+        mutating func visitCodeBlock(_ codeBlock: CodeBlock) {
+            guard let range = codeBlock.range else { return }
+            self.excludedRanges.append(range)
+        }
+    }
+}
+
+
+extension SourceLocation {
+    func index(in string: String) -> String.Index {
+        var idx = string.startIndex
+        var currentLine = 1
+        while currentLine < self.line && idx < string.endIndex {
+            if string[idx] == "\n" {
+                currentLine += 1
+            }
+            idx = string.index(after: idx)
+        }
+        guard let utf8LineStart = idx.samePosition(in: string.utf8) else {
+            return string.endIndex
+        }
+        let byteOffset = self.column - 1
+        let targetUtf8Index = string.utf8.index(utf8LineStart, offsetBy: byteOffset, limitedBy: string.utf8.endIndex) ?? string.utf8.endIndex
+        return targetUtf8Index.samePosition(in: string) ?? string.endIndex
+    }
+}
--- Sources/MarkdownView/Renderers/Math/MathFirstMarkdownViewRenderer.swift
+++ Sources/MarkdownView/Renderers/Math/MathFirstMarkdownViewRenderer.swift
@@ -16,10 +16,9 @@ struct MathFirstMarkdownViewRenderer: MarkdownViewRenderer {
         var configuration = configuration
         var rawText = content.raw.text
         
-        var walker = MathRangeWalker()
-        walker.visit(content.document)
-        let parsingRanges = walker.parsingRanges(text: rawText)
-        for range in parsingRanges {
+        var extractor = ParsingRangesExtractor()
+        extractor.visit(content.document)
+        for range in extractor.parsableRanges(in: rawText) {
             let segment = rawText[range]
             let segmentParser = MathParser(text: segment)
             for math in segmentParser.mathRepresentations.reversed() where !math.kind.inline {
@@ -36,61 +35,5 @@ struct MathFirstMarkdownViewRenderer: MarkdownViewRenderer {
         let _content = MarkdownContent(raw: .plainText(rawText))
         return CmarkFirstMarkdownViewRenderer()
             .makeBody(content: _content, configuration: configuration)
-    }
-}
-
-extension MathFirstMarkdownViewRenderer {
-    struct MathRangeWalker: MarkupWalker {
-        private var excludedRanges: [Range<SourceLocation>] = []
-        
-        func parsingRanges(text: String) -> [Range<String.Index>] {
-            var allowedRanges: [Range<String.Index>] = []
-            let excludedRanges = self.excludedRanges.map {
-                ($0.lowerBound.index(in: text)..<$0.upperBound.index(in: text))
-            }
-
-            let fullRange = text.startIndex..<text.endIndex
-            let sortedExcluded = excludedRanges.sorted { $0.lowerBound < $1.lowerBound }
-            var currentStart = fullRange.lowerBound
-            
-            for ex in sortedExcluded {
-                if currentStart < ex.lowerBound {
-                    allowedRanges.append(currentStart..<ex.lowerBound)
-                }
-                currentStart = ex.upperBound
-            }
-            if currentStart < fullRange.upperBound {
-                allowedRanges.append(currentStart..<fullRange.upperBound)
-            }
-            return allowedRanges
-        }
-        
-        mutating func defaultVisit(_ markup: any Markup) {
-            descendInto(markup)
-        }
-        
-        mutating func visitCodeBlock(_ codeBlock: CodeBlock) {
-            guard let range = codeBlock.range else { return }
-            self.excludedRanges.append(range)
-        }
-    }
-}
-
-extension SourceLocation {
-    func index(in string: String) -> String.Index {
-        var idx = string.startIndex
-        var currentLine = 1
-        while currentLine < self.line && idx < string.endIndex {
-            if string[idx] == "\n" {
-                currentLine += 1
-            }
-            idx = string.index(after: idx)
-        }
-        guard let utf8LineStart = idx.samePosition(in: string.utf8) else {
-            return string.endIndex
-        }
-        let byteOffset = self.column - 1
-        let targetUtf8Index = string.utf8.index(utf8LineStart, offsetBy: byteOffset, limitedBy: string.utf8.endIndex) ?? string.utf8.endIndex
-        return targetUtf8Index.samePosition(in: string) ?? string.endIndex
     }
 }
