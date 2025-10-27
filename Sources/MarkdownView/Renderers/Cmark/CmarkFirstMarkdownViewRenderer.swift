8770952d5775ec5f8322e840b10e305aef9e32c1
Yanan Li <1474700628@qq.com>
2025-10-27 20:53:22 +0800

    Remove cmark renderer result caching

--- Sources/MarkdownView/Renderers/Cmark/CmarkFirstMarkdownViewRenderer.swift
+++ Sources/MarkdownView/Renderers/Cmark/CmarkFirstMarkdownViewRenderer.swift
@@ -13,50 +13,12 @@ struct CmarkFirstMarkdownViewRenderer: MarkdownViewRenderer {
         content: MarkdownContent,
         configuration: MarkdownRendererConfiguration
     ) -> some View {
-        _makeAndCacheBody(
-            content: content,
-            configuration: configuration
-        )
-    }
-    
-    private func _makeAndCacheBody(
-        content: MarkdownContent,
-        configuration: MarkdownRendererConfiguration
-    ) -> some View {
-        if let cached = CacheStorage.shared.withCacheIfAvailable(
-            content,
-            type: Cache.self
-        ), cached.configuration == configuration {
-            return AnyView(cached.renderedView)
-        }
-        
         var parseOptions = ParseOptions()
         if !configuration.allowedBlockDirectiveRenderers.isEmpty {
             parseOptions.insert(.parseBlockDirectives)
         }
         
-        let renderedView = CmarkNodeVisitor(configuration: configuration)
+        return CmarkNodeVisitor(configuration: configuration)
             .makeBody(for: content.parse(options: parseOptions))
-            .erasedToAnyView()
-        
-        CacheStorage.shared.addCache(
-            Cache(
-                markdownContent: content,
-                configuration: configuration,
-                renderedView: renderedView
-            )
-        )
-        
-        return renderedView
-    }
-}
-
-extension CmarkFirstMarkdownViewRenderer {
-    struct Cache: Cacheable {
-        var markdownContent: MarkdownContent
-        var configuration: MarkdownRendererConfiguration
-        var renderedView: any View
-        
-        var cacheKey: some Hashable { markdownContent }
     }
 }
