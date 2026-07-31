8748674265c7fbbe2f724e2af2a85213b5dd4f1a
Paul Ancajima <paul.ancajima@airapps.com>
2026-07-31 12:20:57 -0700

    point to our package

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-markdown.git", from: "0.8.0"),
         .package(url: "https://github.com/raspu/Highlightr.git", from: "2.3.0"),
         .package(url: "https://github.com/mgriebling/SwiftMath.git", from: "1.7.3"),
-        .package(url: "https://github.com/LiYanan2004/RichText.git", from: "1.0.0"),
+        .package(url: "https://github.com/airappsco/RichText-AirApps.git", branch: "main"),
     ],
     targets: [
         .target(
@@ -45,7 +45,7 @@ let package = Package(
                 ),
                 .product(
                     name: "RichText",
-                    package: "RichText",
+                    package: "RichText-AirApps",
                     condition: .when(platforms: [.iOS, .macOS])
                 ),
             ],
@@ -66,7 +66,7 @@ let package = Package(
                 ),
                 .product(
                     name: "RichText",
-                    package: "RichText",
+                    package: "RichText-AirApps",
                     condition: .when(platforms: [.iOS, .macOS])
                 ),
             ],
